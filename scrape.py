759c3c999e051f946d107539e23f33e686cb9e82
jredmon <johntheredmon@gmail.com>
2024-03-24 18:21:44 -0500

    Pulling out hardcoded values to set as params

--- scrape.py
+++ scrape.py
@@ -2,20 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 
 page_url = "https://apnews.com/us-news"
+element_tag = "h3"
+element_class = "PagePromo-title"
+num_headlines = 3
 
 page_to_scrape = requests.get(page_url)
-
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
+def save_headline(headline):
+    print(headline)
+
 if True:
-    headers = soup.findAll("h3", class_="PagePromo-title")
-    #headlines = headers.find("span", attrs={"class":"PagePromoContentIcons-text"}).text
+    headers = soup.findAll(element_tag, class_=element_class)
 
 count = 0
 for header in headers:
-    if count > 3:
+    if count >= num_headlines:
         break
-    print(str(count) + ":" + headline.find("span", class_="PagePromoContentIcons-text").text)
+    headline = header.a.span.text
+    save_headline(headline)
     count = count + 1
-    
-#print(soup.prettify)
+
